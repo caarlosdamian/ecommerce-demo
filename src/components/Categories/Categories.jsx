11e3b6bc84c227b52b85d15fcd9bf276acb73fde
caarlosdamian <dapecarlos@gmail.com>
2021-09-08 23:01:17 -0500

    Feature: Add Category and item category component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { categories } from "../../utils/data";
+import CategoyItem from "./CategoryItem/CategoyItem";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
+const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoyItem item={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default Categories;
